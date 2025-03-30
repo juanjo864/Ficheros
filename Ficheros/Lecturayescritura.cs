a8fa1d8a4264715b829339995823a61c16b3aec9
juanjo864 <zeus977896@gmail.com>
2025-03-30 20:11:32 +0200

    Add files via upload

---
+++ Ficheros/Lecturayescritura.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficheros
+{
+    public class Lecturayescritura
+    {
+        public void escritura()
+        {
+            try
+            {
+                Console.WriteLine("escribe algo en el archivo");
+                String escribire = Console.ReadLine();
+                // en el using lo que hace es que el streamwrite cierre de manera segura
+                //y libere recurso,el streamwriter se utiliza para escribir fichero
+                //dentro del streamwriter devuelve la ruta del directorio actual en el que se está ejecutando el programa
+                using (StreamWriter escribir = new StreamWriter(Directory.GetCurrentDirectory() + "/texto.txt"))
+                {
+                    escribir.WriteLine(escribire);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("error de entrada y salida de fichero");
+            }
+        }
+        public void lectura()
+        {
+            try
+            {
+                
+                // en el using lo que hace es que el streamwReader cierre de manera segura
+                //y libere recurso,el streamReader se utiliza para leerfichero
+                //dentro del streamwriter devuelve la ruta del directorio actual en el que se está ejecutando el programa
+                using (StreamReader lectura = new StreamReader(Directory.GetCurrentDirectory() + "/texto.txt"))
+                {
+                    String lineas;
+                    //lee hasta el final del fichero
+                    while ((lineas=lectura.ReadLine())!=null)
+                    {
+                        Console.WriteLine (lineas);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("error de entrada y salida de fichero");
+            }
+        }
+    }
+}
---
+++ Ficheros/Principal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficheros
+{
+    public class Principal
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Lecturayescritura lecturayescritura = new Lecturayescritura();
+                int numero;
+                do
+                {
+                    Console.WriteLine("elige una de las opciones para practicar ficheros");
+                    Console.WriteLine("1. escritura");
+                    Console.WriteLine("2. lectura");
+                    Console.WriteLine("3. exit");
+                    numero = Convert.ToInt32(Console.ReadLine());
+
+                    switch (numero)
+                    {
+                        case 1:
+                            lecturayescritura.escritura();
+                            break;
+                        case 2:
+                            lecturayescritura.lectura();
+                            break;
+                        case 3:
+                            Environment.Exit(0);
+                             break;
+                        default:
+                            Console.WriteLine("fuera de los limites");
+                            break;
+                    }
+
+                } while (numero != 3);
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("error de entrada y salida");
+            }
+        }
+            
+          
+        
+    }
+}
